12cd36d2fe84c3b6828485ed324eb091a69bcad6
IdanKohavi <idanadik@gmail.com>
2024-12-09 19:26:43 +0200

    New code for both Hello & Name

--- Name.java
+++ Name.java
@@ -1,7 +1,7 @@
 public class Name{
 	public static void main(String[] args){
 		for (int i=1; i <= 10; i++){
-			System.out.println(i + ". Idan Kohavi 207073313");
+			System.out.println(i + ". NEW Idan Kohavi 207073313");
 		}
 	}
 }
